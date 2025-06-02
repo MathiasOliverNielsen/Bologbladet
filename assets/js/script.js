59cfc6942374c685aeb362751b9a9733526b8182
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-06-02 13:47:15 +0200

    Enhance slideshow functionality and styling; add navigation dots and captions for better user interaction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,51 @@
-console.log('Weee! I am a JavaScript file!');
+let slideIndex = 1;
+let autoSlideTimeout;
+
+// Manual next/prev
+function plusSlides(n) {
+  clearTimeout(autoSlideTimeout); // stop auto when user interacts
+  showSlides((slideIndex += n));
+  startAutoSlide();
+}
+
+// Manual thumbnail click
+function currentSlide(n) {
+  clearTimeout(autoSlideTimeout);
+  showSlides((slideIndex = n));
+  startAutoSlide();
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName('mySlides');
+  const dots = document.getElementsByClassName('dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
+}
+
+function startAutoSlide() {
+  autoSlideTimeout = setTimeout(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+    startAutoSlide();
+  }, 10000); // every 10 seconds
+}
+// Start the slideshow
+showSlides(slideIndex);
+startAutoSlide();
